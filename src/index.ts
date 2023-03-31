b53489647f8cecf93c6da388b1826b6617ace8ec
rafaapcode <rafaap2003@gmail.com>
2023-03-31 08:16:07 -0300

    feat: mensagens de erro

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,20 @@ const inputIdBoard = document.querySelector('#idBoard') as HTMLInputElement;
 const tokenBoard = document.querySelector('#tokenBoard') as HTMLInputElement;
 const keyBoard = document.querySelector('#keyBoard') as HTMLInputElement;
 const startbutton = document.querySelector('#startBtn') as HTMLButtonElement;
-const erro = document.querySelector('.error') as HTMLParagraphElement;
+const mensagem = document.querySelector('.mensagem') as HTMLParagraphElement;
 
 Notify.verifyNotification();
 
 startbutton.addEventListener('click', () => {
     if(!inputIdBoard.value || !tokenBoard.value || !keyBoard.value) {
-        erro.classList.remove('hidden');
-        erro.innerHTML = 'Preencha todos os campos !';
+        mensagem.classList.remove('hidden');
+        mensagem.classList.remove('ok');
+        mensagem.classList.add('error');
+        mensagem.innerHTML = 'Preencha todos os campos !';
     } else {
-        erro.classList.add('hidden');
+        mensagem.classList.add('hidden');
+        mensagem.classList.remove('error');
+        mensagem.classList.remove('ok');
     }
     const initialize = new MainFeature(inputIdBoard.value, tokenBoard.value, keyBoard.value);
     initialize.start();
